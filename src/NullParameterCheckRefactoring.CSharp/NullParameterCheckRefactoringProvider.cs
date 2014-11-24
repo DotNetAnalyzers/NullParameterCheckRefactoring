aa5cb37bb3f551ab8bd5c24813e9b40376bf0391
Bart Calixto <Bart@bartmax.com>
2014-11-24 15:09:36 -0200

    Update NullParameterCheckRefactoringProvider.cs

--- src/NullParameterCheckRefactoring.CSharp/NullParameterCheckRefactoringProvider.cs
+++ src/NullParameterCheckRefactoring.CSharp/NullParameterCheckRefactoringProvider.cs
@@ -52,24 +52,11 @@ namespace NullParameterCheckRefactoring
                             .Where(x => x.IsKind(SyntaxKind.EqualsExpression))
                             .Any(expression =>
                             {
-                                bool result = false;
                                 bool isNullCheck = expression.Right.IsKind(SyntaxKind.NullLiteralExpression);
-                                if (isNullCheck)
-                                {
-                                    IdentifierNameSyntax identifierSyntaxt = expression.ChildNodes().OfType<IdentifierNameSyntax>().FirstOrDefault();
-                                    if (identifierSyntaxt != null)
-                                    {
-                                        string identifierText = identifierSyntaxt.Identifier.Text;
-                                        string paramText = parameterSyntax.Identifier.Text;
-
-                                        if (identifierText.Equals(paramText, StringComparison.Ordinal))
-                                        {
-                                            // There is already a null check for this parameter. Skip it.
-                                            result = true;
-                                        }
-                                    }
-                                }
-                                return result;
+                                if (!isNullCheck) return false;
+                                IdentifierNameSyntax identifierSyntaxt = expression.ChildNodes().OfType<IdentifierNameSyntax>().FirstOrDefault();
+                                return (identifierSyntaxt != null && 
+                                    identifierSyntaxt.Identifier.Text.Equals(parameterSyntax.Identifier.Text, StringComparison.Ordinal))
                             });
                     });
 
